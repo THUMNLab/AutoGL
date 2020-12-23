613da209d9eda3709eb7c4cacaec45957038f35b
Frozenmad <frozenmad2015@outlook.com>
2020-12-23 15:52:28 +0800

    change version to 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, THUMNLab/aglteam'
 author = 'THUMNLab/aglteam'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.0'
+release = 'v0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
