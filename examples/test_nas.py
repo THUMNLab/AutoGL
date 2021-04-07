295b6f587faf355ec880a91fa8b666fa4a3a7094
Frozenmad <frozenmad2015@outlook.com>
2021-04-07 13:50:34 +0000

    add test examples

---
+++ examples/test_nas.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('../')
+from autogl.solver import AutoNodeClassifier
+from autogl.module.hpo.nas import BaseNAS, BaseEstimator, GraphSpace
+from autogl.datasets import build_dataset_from_name
+from autogl.module.model import AutoGAT
+from autogl.module.train import NodeClassificationFullTrainer
+from autogl.module.hpo.darts import DartsTrainer
+from torch_geometric.nn import GATConv, GCNConv
+
+class TestNASAlgorithm(BaseNAS):
+    model = None
+    def search(self, space, dset, trainer):
+        num_classes = dset.num_classes
+        num_features = dset.num_features
+        return NodeClassificationFullTrainer(
+            "gat",
+            num_features=num_features,
+            num_classes=num_classes,
+            device="auto"
+        )
+
+if __name__ == '__main__':
+    dataset = build_dataset_from_name('cora')
+    solver = AutoNodeClassifier(
+        feature_module=None,
+        graph_models=[],
+        hpo_module="random",
+        max_evals=10,
+        ensemble_module=None,
+        nas_algorithms=[TestNASAlgorithm()],
+        nas_spaces=[GraphSpace(dataset.num_features, 64, dataset.num_classes, [GATConv, GCNConv])],
+        nas_estimators=[BaseEstimator()]
+    )
+    solver.fit(dataset)
+    out = solver.predict(dataset)
