7d25cde54db9fbcf17bfdac4f30fe627cbb2823c
Frozenmad <frozenmad2015@outlook.com>
2021-06-26 05:50:37 +0000

    add all the modules

--- autogl/__init__.py
+++ autogl/__init__.py
@@ -1,1 +1,18 @@
+from . import (
+    data,
+    datasets,
+    module,
+    solver,
+    utils,
+)
+
+from .module import (
+    ensemble,
+    feature,
+    hpo,
+    model,
+    nas,
+    train,
+)
+
 __version__ = "0.2.0"
