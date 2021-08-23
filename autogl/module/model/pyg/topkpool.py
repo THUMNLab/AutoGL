0ede0c376a7c33e1906a26e20266d808ed486887
SwiftieH <changheng95@126.com>
2021-08-23 07:57:34 +0000

    fixed minor issue in model/pyg

--- autogl/module/model/pyg/topkpool.py
+++ autogl/module/model/pyg/topkpool.py
@@ -4,7 +4,7 @@ from torch_geometric.nn import GraphConv, TopKPooling
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 from . import register_model
 from .base import BaseModel, activate_func
-from ...utils import get_logger
+from ....utils import get_logger
 
 LOGGER = get_logger("TopkModel")
 
