b7cfc618a304137e7043bff2dde3cdb0dc8c4825
lihy96 <lhy237451594@vip.qq.com>
2021-10-13 10:38:19 +0800

    Update __init__.py

--- autogl/__init__.py
+++ autogl/__init__.py
@@ -1,19 +1,19 @@
-# from . import (
-#     backend,
-#     data,
-#     datasets,
-#     module,
-#     solver,
-#     utils,
-# )
-#
-# from .module import (
-#     # ensemble,
-#     # feature,
-#     # hpo,
-#     model,
-#     # nas,
-#     train,
-# )
-#
-# __version__ = "0.2.0-pre"
+from . import (
+    backend,
+    data,
+    datasets,
+    module,
+    solver,
+    utils,
+)
+
+from .module import (
+    # ensemble,
+    # feature,
+    # hpo,
+    model,
+    # nas,
+    train,
+)
+
+__version__ = "0.2.0-pre"
