9a2d22efc978be00ab36c827a40986e38703503e
wondergo2017 <wondergo2017@gmail.com>
2021-11-09 08:18:53 +0000

    resolve conflict

--- test/nas/nclf_dgl.py
+++ test/nas/nclf_dgl.py
@@ -5,7 +5,7 @@ logging.basicConfig(level=logging.INFO)
 
 sys.path.append("../../")
 print(os.getcwd())
-os.environ["AUTOGL_BACKEND"] = "dgl"
+os.environ["AUTOGL_BACKEND"] = "pyg"
 from autogl.backend import DependentBackend
 import dgl
 from dgl.data import CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset
@@ -17,14 +17,13 @@ from autogl.module.nas.space.graph_nas_macro import GraphNasMacroNodeClassificat
 from autogl.module.nas.estimator.one_shot import OneShotEstimator
 from autogl.module.nas.estimator.train_scratch import TrainEstimator
 from autogl.module.nas.backend import bk_feat, bk_label
-from autogl.module.nas.algorithm import Darts,RL,GraphNasRL,Enas,RandomSearch,Spos
+from autogl.module.nas.algorithm import Darts,RL,GraphNasRL,Enas,RandomSearch
+from autogl.module.nas.algorithm import Spos
 from pdb import set_trace
 import numpy as np
 from autogl.datasets import build_dataset_from_name
 from autogl.solver.utils import set_seed
 set_seed(202106)
-
-import nni.retiarii.strategy as strategy
 
 
 if __name__ == '__main__':
