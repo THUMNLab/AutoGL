327f8e9d6a9cf0f384cf1010eb296ae26707dc72
lihy96 <lhy237451594@vip.qq.com>
2021-11-26 10:53:48 +0800

    add split train val test

--- test/link_prediction_trainer.py
+++ test/link_prediction_trainer.py
@@ -78,6 +78,43 @@ def split_train_test(g):
 
     return train_g, train_pos_g, train_neg_g, test_pos_g, test_neg_g
 
+def split_train_valid_test(g):
+    u, v = g.edges()
+
+    eids = np.arange(g.number_of_edges())
+    eids = np.random.permutation(eids)
+
+    valid_size = int(len(eids) * 0.1)
+    test_size = int(len(eids) * 0.1)
+    train_size = g.number_of_edges() - test_size - valid_size
+
+    test_pos_u, test_pos_v = u[eids[:test_size]], v[eids[:test_size]]
+    valid_pos_u, valid_pos_v =  u[eids[test_size:test_size+valid_size]], v[eids[test_size:test_size+valid_size]]
+    train_pos_u, train_pos_v = u[eids[test_size+valid_size:]], v[eids[test_size+valid_size:]]
+
+    # Find all negative edges and split them for training and testing
+    adj = sp.coo_matrix((np.ones(len(u)), (u.numpy(), v.numpy())))
+    adj_neg = 1 - adj.todense() - np.eye(g.number_of_nodes())
+    neg_u, neg_v = np.where(adj_neg != 0)
+
+    neg_eids = np.random.choice(len(neg_u), g.number_of_edges())
+    test_neg_u, test_neg_v = neg_u[neg_eids[:test_size]], neg_v[neg_eids[:test_size]]
+    valid_neg_u, valid_neg_v = neg_u[neg_eids[test_size:test_size+valid_size]], neg_v[neg_eids[test_size:test_size+valid_size]]
+    train_neg_u, train_neg_v = neg_u[neg_eids[train_size:]], neg_v[neg_eids[train_size:]]
+
+    train_g = dgl.remove_edges(g, eids[:test_size+valid_size])
+
+    train_pos_g = dgl.graph((train_pos_u, train_pos_v), num_nodes=g.number_of_nodes())
+    train_neg_g = dgl.graph((train_neg_u, train_neg_v), num_nodes=g.number_of_nodes())
+
+    valid_pos_g = dgl.graph((valid_pos_u, valid_pos_v), num_nodes=g.number_of_nodes())
+    valid_neg_g = dgl.graph((valid_neg_u, valid_neg_v), num_nodes=g.number_of_nodes())
+
+    test_pos_g = dgl.graph((test_pos_u, test_pos_v), num_nodes=g.number_of_nodes())
+    test_neg_g = dgl.graph((test_neg_u, test_neg_v), num_nodes=g.number_of_nodes())
+
+    return train_g, train_pos_g, train_neg_g, valid_pos_g, valid_neg_g, test_pos_g, test_neg_g
+
 if __name__ == "__main__":
 
     setup_seed(1234)
