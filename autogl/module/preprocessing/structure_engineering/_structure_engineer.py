8c4ecce99e225205c574dbf84c51f6f66471f24b
wondergo2017 <wondergo2017@gmail.com>
2022-07-19 12:17:23 +0800

    init

--- autogl/module/preprocessing/structure_engineering/_structure_engineer.py
+++ autogl/module/preprocessing/structure_engineering/_structure_engineer.py
@@ -3,3 +3,19 @@ from .. import _data_preprocessor
 
 class StructureEngineer(_data_preprocessor.DataPreprocessor):
     ...
+
+
+from .._data_preprocessor_registry import DataPreprocessorUniversalRegistry
+from deeprobust.graph.defense.gcn_preprocess import GCNJaccard as Jaccard
+@DataPreprocessorUniversalRegistry.register_data_preprocessor("gcnjaccard")
+class GCNJaccard(StructureEngineer):
+    def __init__(self, threshold=0.01, *args, **kwargs):
+        super(GCNJaccard, self).__init__(*args, **kwargs)
+        self.engine=Jaccard(2,2,2)
+        self.engine.threshold=threshold
+    def _transform(self,data):
+        features=data.x
+        adj=data.edge_index
+        modified_adj = self.engine.drop_dissimilar_edges(features, adj)
+        data.edge_index=modified_adj
+        return data
