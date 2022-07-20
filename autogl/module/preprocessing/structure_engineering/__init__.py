86ffc042f4fbb9c3e605811e3f84a897bbcdf27b
wondergo2017 <wondergo2017@gmail.com>
2022-07-20 19:59:36 +0800

    reorganize; reimplm to_adj_matrix; add test

---
+++ autogl/module/preprocessing/structure_engineering/__init__.py
@@ -0,0 +1,7 @@
+from ._gcn_jaccard import GCNJaccard
+from ._gcn_svd import GCNSVD
+
+__all__ = [
+    GCNJaccard,
+    GCNSVD
+]
--- test/preprocessing/structure.py
+++ test/preprocessing/structure.py
@@ -1,14 +1,17 @@
 envs='dgl pyg'.split()
+forbids=dict(zip(envs,"torch_geometric dgl".split()))
 import os
 from utils import *
+import sys
 
 def func(dev,env):
+    sys.modules[forbids[env]]=None
     os.environ['AUTOGL_BACKEND']=env
     from autogl.backend import DependentBackend
     print('using backend :',DependentBackend.get_backend_name())
     from autogl.datasets import build_dataset_from_name
     data = build_dataset_from_name('cora')
-    from autogl.module.preprocessing.structure_engineering._structure_engineer import GCNJaccard,GCNSVD
+    from autogl.module.preprocessing.structure_engineering import GCNJaccard,GCNSVD
 
     fes=[GCNJaccard,GCNSVD]
     for fe in fes:
@@ -16,8 +19,9 @@ def func(dev,env):
         fe = fe()
         data=fe.fit_transform(data,inplace=False)
 
-    return env
+    return 'Test OK'
 
+# func(0,'dgl')
 results=mp_exec([0,1],envs,func)
 print(results)
 
