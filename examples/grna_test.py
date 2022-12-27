1a4ccd53aa6760efae0c3dd0b632b0c4027f4ab1
BeiniXie <xbn20@mails.tsinghua.edu.cn>
2022-12-27 22:28:32 +0800

    GRNA

--- examples/grna_test.py
+++ examples/grna_test.py
@@ -13,6 +13,33 @@ from tqdm import tqdm
 import numpy as np
 import time
 
+from torch_geometric.utils import to_scipy_sparse_matrix
+from deeprobust.graph.defense import GCN
+from deeprobust.graph.global_attack import Metattack
+
+
+def metattack(data):
+    print('Meta-attack...')
+    adj, features, labels = to_scipy_sparse_matrix(data.edge_index, num_nodes=data.num_nodes), data.x.numpy(), data.y.numpy()
+    idx = np.arange(data.num_nodes)
+    idx_train, idx_val, idx_test = idx[data.train_mask], idx[data.val_mask], idx[data.test_mask]
+    idx_unlabeled = np.union1d(idx_val, idx_test)
+    # Setup Surrogate model
+    surrogate = GCN(nfeat=features.shape[1], nclass=labels.max().item()+1,
+                    nhid=16, dropout=0, with_relu=False, with_bias=False, device='cpu').to('cpu')
+    surrogate.fit(features, adj, labels, idx_train, idx_val, patience=30)
+    # Setup Attack Model
+    model = Metattack(surrogate, nnodes=adj.shape[0], feature_shape=features.shape,
+            attack_structure=True, attack_features=False, device='cpu', lambda_=0).to('cpu')
+    # Attack
+    n_perturbations = int(data.edge_index.size(1)/2 * 0.05)
+    model.attack(features, adj, labels, idx_train, idx_unlabeled, n_perturbations=n_perturbations, ll_constraint=False)
+    perturbed_adj = model.modified_adj
+    perturbed_data = data.clone()
+    perturbed_data.edge_index = torch.LongTensor(perturbed_adj.nonzero())
+
+    return perturbed_data
+
 def test_from_data(trainer, dataset, args):
     for seed in tqdm(range(args.repeat)):
         set_seed(seed)
@@ -30,38 +57,24 @@ if __name__ == '__main__':
     parser.add_argument('--repeat', type=int, default=1)
 
     args = parser.parse_args()
-    device = 'cuda'
-
-    import yaml
-    path_or_dict = yaml.load(open(args.config, "r").read(), Loader=yaml.FullLoader)
-
-    from torch_geometric.datasets import Planetoid
-    dataset_pyg = Planetoid('./data', args.dataset)
-    print('finish loading dataset pyg')
+    device = 'cuda'    
     dataset = build_dataset_from_name(args.dataset, path='./')
-    dataset[0] = dataset_pyg[0]
     
+    print('architecture search')
     solver = AutoNodeClassifier.from_config(args.config)
     solver.fit(dataset)
     solver.get_leaderboard().show()
     orig_acc = solver.evaluate(metric="acc")
-    
     trainer = solver.graph_model_list[0]
     trainer.device = device
 
+    ## test searched model on clean data
+    acc = test_from_data(trainer, dataset, args)
 
     ## test searched model on perturbed data
-    # load perturb data with attacker=mettack
-    modified_adj = torch.from_numpy(np.load(os.path.join('./perturb_data','Mettack', \
-                args.dataset.lower()+'_A_hat_'+str(5)+'.npy')))
-    new_edge_index = torch.nonzero(modified_adj).T.to(device)
-    perturbed_data = dataset[0].clone()
-    perturbed_data.edge_index = new_edge_index
-    dataset[0] = perturbed_data
-    
+    data = dataset[0].cpu()
+    dataset[0] = metattack(data).to(args.device)
     ptb_acc = test_from_data(trainer, dataset, args)
-    # print('acc after poisoning attack:', ptb_acc)
-
 
 
 
