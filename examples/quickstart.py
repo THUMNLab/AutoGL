7277a0d2c223833d9918972549e1bf04050ce51e
generall <general502570@outlook.com>
2022-12-29 23:39:12 +0800

    add setup requier

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,11 +1,10 @@
-
+import pkg_resources
 import autogl
 from autogl.datasets import build_dataset_from_name
 cora_dataset = build_dataset_from_name('cora', path = '~/')
 
 import torch
 device = torch.device('cuda:5' if torch.cuda.is_available() else 'cpu')
-#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 from autogl.solver import AutoNodeClassifier
 solver = AutoNodeClassifier(
     feature_module='deepgl',
@@ -15,10 +14,12 @@ solver = AutoNodeClassifier(
     device=device
 )
 
-solver.fit(cora_dataset, time_limit=3600)
+solver.fit(cora_dataset, time_limit=30)
 solver.get_leaderboard().show()
 
 from autogl.module.train import Acc
+from autogl.solver.utils import get_graph_labels, get_graph_masks
+
 predicted = solver.predict_proba()
-print('Test accuracy: ', Acc.evaluate(predicted, 
-    cora_dataset.data.y[cora_dataset.data.test_mask].cpu().numpy()))
+label = get_graph_labels(cora_dataset[0])[get_graph_masks(cora_dataset[0], 'test')].cpu().numpy()
+print('Test accuracy: ', Acc.evaluate(predicted, label))
